a8b7fd89d813f54aed402c1345e8757094659452
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-16 14:42:55 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FimAlumni::Application.config.secret_token = '8deb75ce20f3a92500f31ccc5ab171bc72bb0fc6cc83cf916204912a55e22b3f51216e117f41cf01d36abfd5550a9b1d688b119f600584bbe526e978b41c7384'
