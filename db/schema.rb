80e52ad4ba577e2ad3743577d01bb7f9b7e0b542
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-16 17:00:19 +0800

    Removed 'name' from User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130216064401) do
+ActiveRecord::Schema.define(:version => 20130216085623) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(:version => 20130216064401) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
