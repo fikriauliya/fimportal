242cc596b0a129b137426f588d2e380d6f7e194d
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-16 17:39:47 +0800

    Removed 'name' from User model 's spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ YAML.load(ENV['ROLES']).each do |role|
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'user: ' << user.email
 user.add_role :admin
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ describe User do
 
   before(:each) do
     @attr = {
-      :name => "Example User",
       :email => "user@example.com",
       :password => "changeme",
       :password_confirmation => "changeme"
