f822b8ce1b4c7a76dd1c30cea67f2723c7d51fec
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-18 09:14:43 +0800

    Geocoder: moved to JS side (from Server side)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,4 @@ class Profile < ActiveRecord::Base
   attr_accessible :batch, :dob, :fullname, :interest, :location, :latitude, :longitude, :motto, :place_of_birth
   validates :fullname, :batch, :presence => true
   belongs_to :user
-  
-  geocoded_by :location   # can also be an IP address
-  after_validation :geocode          # auto-fetch coordinates
 end
