b4a58950b91faafcbe73e7c01dc095cd7033c636
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-19 01:13:32 +0800

    Implemented new layout, allows candidate to apply more than once

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  before_filter :initialize_user
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
 
+  def initialize_user
+    @user = User.new
+  end
 end
--- app/controllers/profile_candidates_controller.rb
+++ app/controllers/profile_candidates_controller.rb
@@ -23,34 +23,34 @@ class ProfileCandidatesController < ApplicationController
   end
 
   def new
-    @profile = ProfileCandidate.find_by_id(current_user.id)
-    if !@profile.nil?
-      redirect_to edit_profile_candidate_path
-    else
-      @profile = ProfileCandidate.new
-  
-      respond_to do |format|
-        format.html # new.html.erb
-        format.json { render json: @profile }
-      end
+    # @profile = ProfileCandidate.find_by_id(current_user.id)
+    # if !@profile.nil?
+      # redirect_to candidate_home_path
+    # else
+    @profile = ProfileCandidate.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @profile }
     end
+    # end
   end
 
   def create
-    if !ProfileCandidate.find_by_id(current_user.id).nil?
-      redirect_to edit_profile_candidate_path
-    else
-      @profile = ProfileCandidate.new(params[:profile_candidate])
-  
-      respond_to do |format|
-        if @profile.save
-          format.html { redirect_to @profile, notice: 'Profile candidate was successfully created.' }
-          format.json { render json: @profile, status: :created, location: @profile }
-        else
-          format.html { render action: "new" }
-          format.json { render json: @profile.errors, status: :unprocessable_entity }
-        end
+    # if !ProfileCandidate.find_by_id(current_user.id).nil?
+      # redirect_to candidate_home_path
+    # else
+    @profile = ProfileCandidate.new(params[:profile_candidate])
+
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to @profile, notice: 'Profile candidate was successfully created.' }
+        format.json { render json: @profile, status: :created, location: @profile }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
       end
     end
+    # end
   end
 end
