285d8199502e74cd4240d52371151fc10f1c3ed1
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-19 08:20:36 +0800

    Validates profile_candidate, precompile

--- app/models/profile_candidate.rb
+++ app/models/profile_candidate.rb
@@ -1,4 +1,6 @@
 class ProfileCandidate < ActiveRecord::Base
   attr_accessible :application_count, :batch, :blood_type, :committee, :dob, :email, :fullname, :gender, :hobby, :information_from, :location, :motivation, :non_formal_education, :organization, :performance_type, :phone, :place_of_birth, :referal, :religion, :school, :workshop, :latitude, :longitude
   belongs_to :user
+  
+  validates :application_count, :batch, :blood_type, :dob, :email, :fullname, :gender, :information_from, :location, :motivation, :phone, :place_of_birth, :religion, :school, :latitude, :longitude, :presence => true
 end
