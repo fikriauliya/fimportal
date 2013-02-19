d42cdb307d761be966d215f6c0b49078a1ef3edd
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-02-19 21:55:10 +0800

    Clustering Google Map's markers. Refactored Google Map loader

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,34 @@
 class HomeController < ApplicationController
   def index
-    @profiles = Profile.all
+    @profiles = ProfileCandidate.all
+    initialize_latitudes_longitudes(@profiles)
   end
   
   def index_alumni
     authenticate_user!
     @profiles = Profile.all
     @profile = Profile.find_by_id(current_user.id)
+    initialize_latitudes_longitudes(@profiles)
   end
   
   def index_candidate
     authenticate_user!
     @profiles = ProfileCandidate.all
     @profile = ProfileCandidate.find_by_id(current_user.id)
+    initialize_latitudes_longitudes(@profiles)
+  end
+  
+  private
+  
+  def initialize_latitudes_longitudes(profiles)
+    @latitudes = []
+    @longitudes = []
+   
+    profiles.each do |p|
+      unless p.latitude.nil? or p.longitude.nil?
+        @latitudes << p.latitude
+        @longitudes << p.longitude
+      end
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,40 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
-
+  
+  def map_loader(latitudes, longitudes)
+    javascript = <<-JAVASCRIPT
+    function initialize() {
+      var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(0, 120.644),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+      
+      #{
+        varLat = "var varLat = [#{latitudes.join(',')}];"
+        varLng = "var varLng = [#{longitudes.join(',')}];"
+        varLat + varLng
+      }
+      
+      var markers = [];
+      for (var i = 0; i < varLat.length; i++) {
+        var newLatLng = new google.maps.LatLng(varLat[i], varLng[i]);
+        var marker = new google.maps.Marker({position: newLatLng,map: map});
+        markers.push(marker);
+      }
+      var markerCluster = new MarkerClusterer(map, markers);
+    }
+    
+    function loadScript() {
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyCEY0Wtv2sMZZRW0Fb3hmutA-7ET0O-58o&sensor=true&callback=initialize";
+      document.body.appendChild(script);
+    }
+    
+    window.onload = loadScript;
+    JAVASCRIPT
+  end
 end
