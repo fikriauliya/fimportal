3a6226cd54262a3f5c1477e6562b09572fac918b
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-02 16:31:05 +0800

    Autocomplete school using typeahead

--- app/helpers/profile_candidates_helper.rb
+++ app/helpers/profile_candidates_helper.rb
@@ -1,2 +1,5 @@
 module ProfileCandidatesHelper
+  def all_school_json
+    "[" + ProfileCandidate.all_school.collect{|s| '"' + s + '"'}.join(",") + "]"
+  end
 end
--- app/models/profile_candidate.rb
+++ app/models/profile_candidate.rb
@@ -17,4 +17,26 @@ class ProfileCandidate < ActiveRecord::Base
   validates_format_of :agreement, :with => lambda {|me|
      /\s*Saya,\s*#{me.fullname} \s*menyatakan bahwa keterangan di atas diisi dengan sebenar-benarnya dan saya bersedia mengikuti seluruh rangkaian kegiatan pelatihan FIM 14 pada tanggal 2-5 Mei 2013\s*/i
    }, :message => "Tidak sama dengan contoh di atas. Coba periksa nama Anda terisi dengan benar."
+   
+  def self.all_school
+    ['Institut Manajemen Telkom', 'Institut Pertanian Bogor', 'Institut Teknologi Bandung', 
+      'Institut Teknologi Sepuluh Nopember', 'Institut Teknologi Telkom', 'Sampoerna School of Education', 
+      'Sekolah Tinggi Akuntansi Negara', 'Sekolah Tinggi Ekonomi Islam Tazkia', 'STMIK Amikom', 'Universitas Airlangga', 
+      'Universitas Al Azhar Indonesia', 'Universitas Andalas', 'Universitas Bakrie', 'Universitas Bangka Belitung', 
+      'Universitas Bengkulu', 'Universitas Brawijaya', 'Universitas Cendrawasih', 'Universitas Diponegoro', 
+      'Universitas Gadjah Mada', 'Universitas Haluoleo', 'Universitas Hasanuddin', 'Universitas Indonesia',
+      'Universitas Islam Negeri Alauddin', 'Universitas Islam Negeri Syarif Hidayatullah', 'Universitas Jambi', 
+      'Universitas Jember', 'Universitas Jenderal Soedirman', 'Universitas Khairun', 'Universitas Lambung Mangkurat', 
+      'Universitas Lampung', 'Universitas Muhammadiyah Makassar', 'Universitas Muhammadiyah Malang', 
+      'Universitas Muhammadiyah Purwokerto', 'Universitas Muhammadiyah Yogyakarta', 'Universitas Mulawarman', 
+      'Universitas Multimedia Nusantara', 'Universitas Negeri Gorontalo', 'Universitas Negeri Jakarta', 
+      'Universitas Negeri Makassar', 'Universitas Negeri Malang', 'Universitas Negeri Manado', 
+      'Universitas Negeri Medan', 'Universitas Negeri Padang', 'Universitas Negeri Papua', 
+      'Universitas Negeri Semarang', 'Universitas Negeri Surabaya', 'Universitas Negeri Surakarta (Sebelas Maret)', 
+      'Universitas Negeri Yogyakarta', 'Universitas Nusa Cendana', 'Universitas Padjadjaran', 
+      'Universitas Palangka Raya', 'Universitas Paramadina', 'Universitas Pattimura', 'Universitas Pendidikan Ganesha', 
+      'Universitas Pendidikan Indonesia', 'Universitas Riau', 'Universitas Sam Ratulangi', 'Universitas Sriwijaya', 
+      'Universitas Sultan Ageng Tirtayasa', 'Universitas Sumatera Utara', 'Universitas Syiah Kuala', 
+      'Universitas Tadulako', 'Universitas Tanjungpura', 'Universitas Trisakti', 'Universitas Udayana']
+  end
 end
