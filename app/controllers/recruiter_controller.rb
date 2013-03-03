5663dc7d0b3b960fdd91a9eb82c7135cd48182ce
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-03 11:51:33 +0800

    Excel download for profile candidates

--- app/controllers/recruiter_controller.rb
+++ app/controllers/recruiter_controller.rb
@@ -2,13 +2,20 @@ class RecruiterController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    authorize! :update, ProfileCandidate, :message => 'Not authorized as an administrator.'
+    authorize! :update, ProfileCandidate, :message => 'Not authorized as a recruiter.'
 
-    @profiles = ProfileCandidate.where(:status => 'SUBMITTED').paginate(:page => params[:page],:per_page => 20)
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @profiles }
+    if params[:format] == "xls"
+      @profiles = ProfileCandidate.where(:status => 'SUBMITTED')
+      respond_to do |format|
+        format.xls
+      end
+    else
+      @profiles = ProfileCandidate.where(:status => 'SUBMITTED').paginate(:page => params[:page],:per_page => 20)
+      
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @profiles }
+      end
     end
   end
 end
