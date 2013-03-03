17bbc2ae620903238952fa2c8346ef1d05fa0397
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-04 07:35:16 +0800

    Only display location of visible users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @profiles = ProfileCandidate.where(:status => 'SUBMITTED').select([:latitude, :longitude])
+    @profiles = ProfileCandidate.where(:status => 'SUBMITTED', :is_visible_to_public => true).select([:latitude, :longitude])
     initialize_latitudes_longitudes(@profiles)
   end
 end
