e9130815142977a3c615954cc06c16028289eb97
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-06 09:01:50 +0800

    will_paginate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.all
+    @users = User.order("created_at ASC").paginate(:page => params[:page],:per_page => 20)
   end
 
   def show
