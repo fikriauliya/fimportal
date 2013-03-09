bc85ade551a025b941f63014a5a2605626d5dba3
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-09 13:59:21 +0800

    Small refactoring 'submitted'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @profiles = ProfileCandidate.where(:status => 'SUBMITTED', :is_visible_to_public => true).select([:latitude, :longitude])
+    @profiles = ProfileCandidate.submitted.where(:is_visible_to_public => true).select([:latitude, :longitude])
     initialize_latitudes_longitudes(@profiles)
   end
 end
