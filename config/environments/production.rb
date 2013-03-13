5243072a4b7255532b824f459b912cb011d7c7a1
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-13 16:01:37 +0800

    Change Gmail SMTP Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ FimAlumni::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'forumindonesiamuda.org' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -72,7 +72,7 @@ FimAlumni::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
