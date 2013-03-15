d992d6bd714af06cb3c277a386bff459061233ec
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-15 09:11:45 +0800

    Add province breakdown in statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,5 +3,7 @@ class StatisticsController < ApplicationController
   
   def index
     authorize! :index, User, :message => 'Not authorized as an administrator.'
+    @provinces_count = ProfileCandidate.count(:all, :group => :province, :order => 'count_all DESC')
+    @unexist_provinces = ProfileCandidate.all_province - @provinces_count.collect{|p| p[0]} 
   end
 end
