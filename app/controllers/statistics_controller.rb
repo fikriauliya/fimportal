6ebb70435aeea2fe4df716808851c0239597ebc6
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-15 12:01:03 +0800

    Add school grouping in statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -5,5 +5,6 @@ class StatisticsController < ApplicationController
     authorize! :index, User, :message => 'Not authorized as an administrator.'
     @provinces_count = ProfileCandidate.count(:all, :group => :province, :order => 'count_all DESC')
     @unexist_provinces = ProfileCandidate.all_province - @provinces_count.collect{|p| p[0]} 
+    @schools_count = ProfileCandidate.count(:all, :group => :school, :order => 'count_all DESC')
   end
 end
