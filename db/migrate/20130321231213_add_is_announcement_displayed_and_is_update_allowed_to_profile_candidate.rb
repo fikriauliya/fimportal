9d7750281273b9022a261f17352eba5391ee4386
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-03-22 22:08:43 +0800

    Fix migration update_all for pg

--- db/migrate/20130321231213_add_is_announcement_displayed_and_is_update_allowed_to_profile_candidate.rb
+++ db/migrate/20130321231213_add_is_announcement_displayed_and_is_update_allowed_to_profile_candidate.rb
@@ -3,8 +3,8 @@ class AddIsAnnouncementDisplayedAndIsUpdateAllowedToProfileCandidate < ActiveRec
     add_column :profile_candidates, :is_announcement_displayed, :boolean, :default => false
     add_column :profile_candidates, :is_update_allowed, :boolean, :default => false
     
-    ProfileCandidate.update_all('is_announcement_displayed = 1', "status = 'NOT SUBMITTED'")
-    ProfileCandidate.update_all('is_update_allowed = 1', "status = 'MARKED' OR status = 'SUBMITTED'")
+    ProfileCandidate.update_all('is_announcement_displayed = true', "status = 'NOT SUBMITTED'")
+    ProfileCandidate.update_all('is_update_allowed = true', "status = 'MARKED' OR status = 'SUBMITTED'")
   end
   
   def down
