67fd783b984591da3513feeb7bedc853e7c79c31
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-04-17 21:59:12 +0800

    Removed unused DB query in home. Add back link to profile candidates in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
     (@latitudes, @longitudes) = get_profile_candidates_latitudes_longitudes
-    @accepted_cibubur = ProfileCandidate.where(:is_accepted => true, :accepted_location => 'Cibubur')
-    @accepted_bukittinggi = ProfileCandidate.where(:is_accepted => true, :accepted_location => 'Bukit Tinggi')
   end
   
   def closed
