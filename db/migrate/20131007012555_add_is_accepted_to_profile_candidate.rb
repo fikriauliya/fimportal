80deb44a80cfd6556e2f7a3b3eb7675e7b3e5db8
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-10-07 10:55:07 +0800

    add_is_accepted migration to check the existance of column first

--- db/migrate/20131007012555_add_is_accepted_to_profile_candidate.rb
+++ db/migrate/20131007012555_add_is_accepted_to_profile_candidate.rb
@@ -1,5 +1,11 @@
 class AddIsAcceptedToProfileCandidate < ActiveRecord::Migration
-  def change
-    add_column :profile_candidates, :is_accepted, :boolean, :default => false
+  def up
+  	ProfileCandidate.reset_column_information
+    add_column :profile_candidates, :is_accepted, :boolean, :default => false unless ProfileCandidate.column_names.include?('is_accepted')
+  end
+
+  def down
+  	ProfileCandidate.reset_column_information
+  	remove_column :profile_candidates, :is_accepted if ProfileCandidate.column_names.include?('is_accepted')
   end
 end
