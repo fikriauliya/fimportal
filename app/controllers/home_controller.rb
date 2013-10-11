d92846246bc83748add5275033d55d5e07a07a6d
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-10-12 00:08:22 +0800

    Auto redirect root to WP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     (@latitudes, @longitudes) = get_profile_candidates_latitudes_longitudes
+    redirect_to 'http://wp.forumindonesiamuda.org/category/fim-15/'
   end
 
   def faq
