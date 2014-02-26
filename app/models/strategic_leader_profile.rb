66fe53514d0ee23f6799a7e76823e4d43862309b
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-27 06:41:48 +0700

    unfinish validator in strategic_leader

--- app/models/strategic_leader_profile.rb
+++ app/models/strategic_leader_profile.rb
@@ -1,5 +1,8 @@
 class StrategicLeaderProfile < ActiveRecord::Base
   attr_accessible :activity1_information, :activity1_reason, :activity2_information, :activity2_reason, :activity3_information, :activity3_reason, :activity4_information, :activity4_reason, :activity5_information, :activity5_reason, :organization_information, :essay_about_indonesia
   
+
   belongs_to :user
+
+  #validates :activity1_information, :activity1_reason, :activity2_information, :activity2_reason, :activity3_information, :activity3_reason, :activity4_information, :activity4_reason, :activity5_information, :activity5_reason, :organization_information, :essay_about_indonesia => true
 end
