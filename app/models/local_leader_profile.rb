ab795b8181cb9cc59d096153944d42b3d8f0a327
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-27 05:19:46 +0700

    minor change in model local_leader_profile

--- app/models/local_leader_profile.rb
+++ app/models/local_leader_profile.rb
@@ -1,3 +1,5 @@
 class LocalLeaderProfile < ActiveRecord::Base
-  attr_accessible :essay_about_organization, :organization_information, :user_id
+  attr_accessible :essay_about_organization, :organization_information
+
+  belongs_to :user
 end
