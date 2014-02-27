4eab884cc869c0d707e1fce6575ddb1f21e894b1
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-27 22:26:19 +0700

    add validator in activist model

--- app/models/activist_profile.rb
+++ app/models/activist_profile.rb
@@ -10,4 +10,9 @@ class ActivistProfile < ActiveRecord::Base
   	:activity2_reason, :activity3_information, :activity3_reason, :activity4_information, 
   	:activity4_reason, :activity5_information, :activity5_reason, :essay_about_pasca_training, 
   	:tipografi, :presence => true
+
+  validates_length_of :activity1_reason, :activity2_reason, :activity3_reason, :activity4_reason, 
+  	:activity5_reason, :too_long => 'Terlalu panjang, melebihi 350 karakter', :maximum => 350
+
+  validates_length_of :essay_about_pasca_training, :too_long => 'Terlalu panjang, melebihi 1000 kata', :maximum => 350
 end
