f9bb221642788a851025258545d5b283fb0ea230
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-27 21:58:52 +0700

    add validator for local leader model

--- app/models/strategic_leader_profile.rb
+++ app/models/strategic_leader_profile.rb
@@ -8,4 +8,7 @@ class StrategicLeaderProfile < ActiveRecord::Base
   	:activity2_information, :activity2_reason, :activity3_information, 
   	:activity3_reason, :activity4_information, :activity4_reason, :activity5_information, 
   	:activity5_reason, :organization_information, :essay_about_indonesia, :presence => true
+
+  validates_length_of :activity1_reason, :activity2_reason, :activity3_reason, :activity4_reason, 
+  	:activity5_reason, :too_long => 'Terlalu panjang, melebihi 350 karakter', :maximum => 350
 end
