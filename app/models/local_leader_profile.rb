5f25fa207c3691785675170cbee3a89478e71251
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-27 21:48:59 +0700

    add validator for local leader model

--- app/models/local_leader_profile.rb
+++ app/models/local_leader_profile.rb
@@ -2,4 +2,6 @@ class LocalLeaderProfile < ActiveRecord::Base
   attr_accessible :essay_about_organization, :organization_information
 
   belongs_to :user
+
+  validates :essay_about_organization, :organization_information, :presence => true
 end
