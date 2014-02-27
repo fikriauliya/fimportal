5dc4f8dfc48f09568deba8c2158cb74bf17598f4
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-27 21:34:30 +0700

    minor wording button in home

--- app/models/strategic_leader_profile.rb
+++ app/models/strategic_leader_profile.rb
@@ -4,5 +4,8 @@ class StrategicLeaderProfile < ActiveRecord::Base
 
   belongs_to :user
 
-  #validates :activity1_information, :activity1_reason, :activity2_information, :activity2_reason, :activity3_information, :activity3_reason, :activity4_information, :activity4_reason, :activity5_information, :activity5_reason, :organization_information, :essay_about_indonesia => true
+  validates :activity1_information, :activity1_reason, 
+  	:activity2_information, :activity2_reason, :activity3_information, 
+  	:activity3_reason, :activity4_information, :activity4_reason, :activity5_information, 
+  	:activity5_reason, :organization_information, :essay_about_indonesia, :presence => true
 end
