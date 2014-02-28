fa966332dfa782454d4969ba9ad83ecc1a416fce
ihsan satriawan <ihsan.satriawan.20@gmail.com>
2014-02-28 13:31:12 +0700

    add field for local_leader step

--- app/models/local_leader_profile.rb
+++ app/models/local_leader_profile.rb
@@ -1,14 +1,14 @@
 class LocalLeaderProfile < ActiveRecord::Base
   attr_accessible :essay_about_organization, :organization_information, :organization_name,
    :organization_year, :organization_category, :organization_activity, 
-   :organization_count_member, :organization_goalorganization_achievement,
+   :organization_count_member, :organization_goal, :organization_achievement,
    :organization_contact, :organization_network
 
   belongs_to :user
 
   validates :essay_about_organization, :organization_information, :organization_name,
    :organization_year, :organization_category, :organization_activity, 
-   :organization_count_member, :organization_goalorganization_achievement,
+   :organization_count_member, :organization_goal, 
    :organization_contact, :organization_network, :presence => true
 
   validates_length_of :essay_about_organization, :too_long => 'Terlalu panjang, melebihi 1000 karakter', :maximum => 1000
